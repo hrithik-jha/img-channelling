ca4a2a25fb6b2f6bbbf363f7125baae6b2f396c3
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2020-03-23 12:14:44 +0530

    Change tar to individual req

--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ import os
 import cv2
 
 
-target_path = "channels.tar.gz"
+target_path = "channelsC.tar.gz"
 
 def sendImage(i):
     image_filename = os.path.basename(i)
@@ -13,11 +13,9 @@ def sendImage(i):
     response = requests.post('http://localhost:5000/upload', files=multipart_form_data)
     print(response)
 
-    if response.status_code == 200:
-        with open(target_path, 'wb') as f:
-            f.write(response.raw.read())
+    
 
 
 
-file = "./file.png"
-sendImage("./file.png")
+file = "./file.jpg"
+sendImage("./file.jpg")
