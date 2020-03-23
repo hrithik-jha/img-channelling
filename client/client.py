14f738fb33f1c2a13ceff989a6c1cbf90cde27c1
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2020-03-23 19:26:35 +0530

    Complete functionality

--- client/client.py
+++ client/client.py
@@ -2,18 +2,18 @@ import requests
 import os
 import cv2
 import shutil
+import numpy as np
 
 
 def mergeChannels():
     print("Merging...")
-    b = cv2.imread('blue.jpg')
-    r = cv2.imread('red.jpg')
-    g = cv2.imread('green.jpg')
+    b = cv2.imread('blue.jpg', cv2.IMREAD_GRAYSCALE)
+    r = cv2.imread('red.jpg', cv2.IMREAD_GRAYSCALE)
+    g = cv2.imread('green.jpg', cv2.IMREAD_GRAYSCALE)
 
-    #image = cv2.merge((b, g, r))
-    image = cv2.merge([b, g, r]);
-    merged = cv2.cvtColor(image,cv2.CV_HSV2BGR);
-    cv2.imwrite('merged.jpg', merged)
+    image = cv2.merge((b, g, r))
+    print(image.shape)
+    cv2.imwrite('merged.jpg', image)
     
 def sendImage(i):
     image_filename = os.path.basename(i)
@@ -23,11 +23,11 @@ def sendImage(i):
     response = requests.post('http://localhost:5000/upload', files=multipart_form_data)
     print(response)
 
-
+    url = 'http://localhost:5000/images?col='
     queries = ['red', 'green', 'blue']
     if response.status_code == 200:
         for i in queries:
-            response = requests.get('http://localhost:5000/images?col=' + i, stream=True)
+            response = requests.get(url + i, stream=True)
             with open(i+'.jpg', 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
     mergeChannels()
